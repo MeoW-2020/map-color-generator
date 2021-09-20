9f32fe96f50410c8354fd9ede9da56677efec5ad
MeoW <89231009+MeoW-2020@users.noreply.github.com>
2021-09-20 21:49:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var rng = document.getElementsByClassName('rng');
+var rngH = document.getElementById('rng-h');
+var rngS = document.getElementById('rng-s');
+var rngL = document.getElementById('rng-l');
+
+var numH = document.getElementById('num-h');
+var numS = document.getElementById('num-s');
+var numL = document.getElementById('num-l');
+
+
+rngH.onchange = rngH.oninput = function () {
+    document.documentElement.style.setProperty('--hue', this.value);
+    document.documentElement.style.setProperty('--hue-secondary', this.value - 3);
+    numH.innerHTML = rngH.value;
+}
+rngS.onchange = rngS.oninput = function () {
+    document.documentElement.style.setProperty('--s', this.value + "%");
+    document.documentElement.style.setProperty('--s-secondary', (this.value - 27) + "%");
+    numS.innerHTML = rngS.value;
+}
+rngL.onchange = rngL.oninput = function () {
+    document.documentElement.style.setProperty('--l', this.value + "%");
+    document.documentElement.style.setProperty('--l-secondary', (this.value - 46) + "%");
+    numL.innerHTML = rngL.value;
+
+    if (this.value < 50) {
+        document.documentElement.style.setProperty('--text-color', '#ffffff');
+    } else {
+        document.documentElement.style.setProperty('--text-color', '#000000');
+    }
+}
